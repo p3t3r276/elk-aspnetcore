b2e404caa9555c1d798ca0d0e9cac78a01875a0d
Khoa Le Minh <p3t3r276@gmail.com>
2024-10-01 09:17:00 +0700

    Push log to ELK.

--- ELKExample/Program.cs
+++ ELKExample/Program.cs
@@ -1,20 +1,35 @@
 
 using Serilog;
 using Serilog.Exceptions;
+using Serilog.Formatting.Json;
 using Serilog.Sinks.Elasticsearch;
+using Serilog.Sinks.File;
 using System.Reflection;
+using static Serilog.Events.LogEventLevel;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+ConfigureLogging();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-ConfigureLogging();
 builder.Host.UseSerilog();
+// builder.Host.UseSerilog((t, f) => f.Enrich.FromLogContext().ReadFrom.Configuration(t.Configuration));
+// builder.Host.UseSerilog((context, services, configuration) => {
+//     configuration.Enrich.FromLogContext()
+//         .ReadFrom.Configuration(context.Configuration)
+//         .Enrich.WithExceptionDetails()
+//         .WriteTo.Debug()
+//         .WriteTo.Console()
+//         .WriteTo.Elasticsearch(ConfigureEslasticSink(context.Configuration, context.HostingEnvironment.EnvironmentName))
+//         .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+//         .ReadFrom.Configuration(context.Configuration);
+// });
 
 var app = builder.Build();
 
@@ -43,6 +58,13 @@ void ConfigureLogging()
         .Build();
 
     Log.Logger = new LoggerConfiguration()
+    #if DEBUG
+            .MinimumLevel.Debug()
+#else
+            .MinimumLevel.Information()
+#endif
+        .MinimumLevel.Override("Microsoft", Information)
+        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", Warning)
         .Enrich.FromLogContext()
         .Enrich.WithExceptionDetails()
         .WriteTo.Debug()
@@ -55,11 +77,19 @@ void ConfigureLogging()
 
 static  ElasticsearchSinkOptions ConfigureEslasticSink(IConfigurationRoot configuration, string environment)
 {
+    Console.WriteLine($"{Assembly.GetExecutingAssembly().GetName().Name?.ToLower().Replace(".", "-")}");
     return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
     {
-        AutoRegisterTemplate = true,
         IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name?.ToLower().Replace(".", "-")} - {environment.ToLower()} - {DateTime.UtcNow:yyyy-MM}",
-        NumberOfReplicas = 2,
-        NumberOfShards = 2
+        TemplateName="serilog-events-template",
+        AutoRegisterTemplate = true,
+        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+        BatchAction = ElasticOpType.Create, 
+        TypeName = null,
+        FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
+        EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                                        EmitEventFailureHandling.WriteToFailureSink |
+                                        EmitEventFailureHandling.RaiseCallback,
+        FailureSink = new FileSink("./failures.json", new JsonFormatter(), null)
     };
 }
